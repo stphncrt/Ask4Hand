d4c25cb3038ce41761a29bc32b1cbd18c4f11bbd
ayktldg <aykutuludag26@gmail.com>
2022-06-01 16:59:23 +0200

    add validation

---
+++ server/src/util/authValidation.js
@@ -0,0 +1,34 @@
+import Joi from "joi";
+
+export const registerValidation = (data) => {
+  const schema = Joi.object({
+    firstName: Joi.string().min(3).max(30).required(),
+    lastName: Joi.string().min(3).max(30).required(),
+    title: Joi.string().min(3).max(30).required(),
+    email: Joi.string().email().required(),
+    phoneNumber: Joi.number().required(),
+    birthYear: Joi.number().integer().min(1900).max(2013),
+    profileImage: Joi.string(),
+    categoryId: Joi.string().required(),
+    description: Joi.string().required().max(300),
+    street: Joi.string().required(),
+    houseNumber: Joi.string().required(),
+    postalCode: Joi.string().required(),
+    city: Joi.string().required(),
+    location: Joi.object(),
+    hourlyRate: Joi.number().integer(),
+    workRange: Joi.number().integer(),
+    password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+  });
+  const validation = schema.validate(data);
+  return validation;
+};
+
+export const loginValidation = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+  });
+  const validation = schema.validate(data);
+  return validation;
+};
