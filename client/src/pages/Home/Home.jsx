8cae1315f04428f1c222f730e6fcdf846f096ce7
ensar dogan <doganermansur@icloud.com>
2022-06-01 22:56:10 +0200

    navbar created

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-
+import { NavBar } from "../../components/Nav";
 import TEST_ID from "./Home.testid";
 
 const Home = () => {
   return (
     <div data-testid={TEST_ID.container}>
-      <h1>This is the homepage</h1>
-      <p>Good luck with the project!</p>
+      <NavBar />
     </div>
   );
 };
