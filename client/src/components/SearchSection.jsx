5d75bc248e7861c6659a7e3d195f0e9c88611262
ensar dogan <doganermansur@icloud.com>
2022-06-02 11:41:26 +0200

    search bar created

---
+++ client/src/components/SearchSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+const SearchSection = () => {
+  return <StyledContainer> SearchSection </StyledContainer>;
+};
+
+export default SearchSection;
+
+export const StyledContainer = styled.div`
+  height: 100px;
+  background-color: red;
+`;
