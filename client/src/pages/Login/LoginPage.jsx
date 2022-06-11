d6d1c9c99d573446619eeeff03ca7a3cf3e317d8
ensar dogan <doganermansur@icloud.com>
2022-06-11 19:00:06 +0200

    sent data to database

--- client/src/pages/Login/LoginPage.jsx
+++ client/src/pages/Login/LoginPage.jsx
@@ -47,7 +47,6 @@ const LoginPage = () => {
     },
     validationSchema: LoginValidationSchema,
     onSubmit: (values) => {
-      console.log(values);
       postWorker("/auth/login", values);
     },
   });
