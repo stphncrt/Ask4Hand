7d260bce0be5deb1a1c5e5dfe20597482bebdeb9
stphn <cretestephen@gmail.com>
2022-06-15 22:39:09 +0200

    searchPage styles added

--- client/src/components/TitleCheckBox.jsx
+++ client/src/components/TitleCheckBox.jsx
@@ -9,66 +9,64 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 
 export default function CheckboxLabels({ occupationId, categoryId }) {
-  const { titles, getTitles, isLoading } = useContext(AppContext);
-  const [city, setCity] = useState("");
-  const [checkedTitles, setCheckedTitles] = useState([]);
+	const { titles, getTitles, isLoading } = useContext(AppContext);
+	const [city, setCity] = useState("");
+	const [checkedTitles, setCheckedTitles] = useState([]);
+	const handleFilterClick = () => {
+		console.log(city, checkedTitles);
+	};
 
-  const handleFilterClick = () => {
-    console.log(city, checkedTitles);
-  };
+	useEffect(() => {
+		getTitles("/occupations");
+	}, []);
 
-  useEffect(() => {
-    getTitles("/occupations");
-  }, []);
-
-  return (
-    <StyledFormGroup>
-      <div>
-        {isLoading ? (
-          <h3>Loading..</h3>
-        ) : (
-          titles?.map((title) => (
-            <FormControlLabel
-              key={title._id}
-              onChange={(e) =>
-                setCheckedTitles([...checkedTitles, e.target.value])
-              }
-              control={
-                (categoryId && title.categoryId === categoryId) ||
-                title._id === occupationId ? (
-                  <Checkbox checked value={title._id} />
-                ) : (
-                  <Checkbox value={title._id} />
-                )
-              }
-              label={title.name}
-            />
-          ))
-        )}
-      </div>
-      <TextField
-        id="outlined-basic"
-        label="Outlined"
-        variant="outlined"
-        onChange={(e) => setCity(e.target.value)}
-      />
-      <Button variant="contained" onClick={handleFilterClick}>
-        Contained
-      </Button>
-    </StyledFormGroup>
-  );
+	return (
+		<StyledFormGroup>
+			<div>
+				{isLoading ? (
+					<h3>Loading..</h3>
+				) : (
+					titles?.map((title) => (
+						<FormControlLabel
+							key={title._id}
+							onChange={(e) => setCheckedTitles([...checkedTitles, e.target.value])}
+							control={
+								(categoryId && title.categoryId === categoryId) || title._id === occupationId ? (
+									<Checkbox checked value={title._id} />
+								) : (
+									<Checkbox value={title._id} />
+								)
+							}
+							label={title.name}
+						/>
+					))
+				)}
+			</div>
+			<TextField
+				id="outlined-basic"
+				label="City"
+				variant="outlined"
+				onChange={(e) => setCity(e.target.value)}
+			/>
+			<Button variant="contained" onClick={handleFilterClick}>
+				Contained
+			</Button>
+		</StyledFormGroup>
+	);
 }
 
 export const StyledFormGroup = styled(FormGroup)`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  margin: 1rem;
-  border: 1px solid;
-  width: 35%;
-  padding: 0.2rem;
-  div {
-    display: flex;
-    flex-direction: column;
-  }
+	display: flex;
+	flex-direction: column;
+	height: 60vh;
+	overflow-y: scroll;
+	gap: 1rem;
+	margin: 1rem;
+	border: 1px solid;
+	width: 50vh;
+	padding: 0.2rem;
+	div {
+		display: flex;
+		flex-direction: column;
+	}
 `;
--- client/src/pages/SearchPage/SearchPage.jsx
+++ client/src/pages/SearchPage/SearchPage.jsx
@@ -5,42 +5,46 @@ import styled from "styled-components";
 import TitleCheckBox from "../../components/TitleCheckBox";
 
 function SearchPage() {
-  const location = useLocation().state;
-  const { workerList, getWorkerByTitle, getWorkerByCategory, isLoading } =
-    useContext(AppContext);
+	const location = useLocation().state;
+	const { workerList, getWorkerByTitle, getWorkerByCategory, isLoading } = useContext(AppContext);
 
-  useEffect(() => {
-    if (location.categoryId) {
-      getWorkerByCategory(`/find/${location.categoryId}`);
-    }
-    if (location.occupationId) {
-      getWorkerByTitle(`/search/${location.occupationId}`);
-    }
-  }, []);
+	useEffect(() => {
+		if (location.categoryId) {
+			getWorkerByCategory(`/find/${location.categoryId}`);
+		}
+		if (location.occupationId) {
+			getWorkerByTitle(`/search/${location.occupationId}`);
+		}
+	}, []);
 
-  return (
-    <StyledWrapper>
-      <TitleCheckBox
-        occupationId={location.occupationId}
-        categoryId={location.categoryId}
-      />
-      <div>
-        {isLoading ? (
-          <h3>Loading...</h3>
-        ) : (
-          workerList?.map((worker) => (
-            <p key={worker._id}>{worker.firstName}</p>
-          ))
-        )}
-      </div>
-    </StyledWrapper>
-  );
+	return (
+		<StyledWrapper>
+			<TitleCheckBox occupationId={location.occupationId} categoryId={location.categoryId} />
+			<div className="container">
+				<div className="map">Map</div>
+				{isLoading ? (
+					<h3>Loading...</h3>
+				) : (
+					workerList?.map((worker) => <p key={worker._id}>{worker.firstName}</p>)
+				)}
+			</div>
+		</StyledWrapper>
+	);
 }
 
 export default SearchPage;
 export const StyledWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  padding: 3rem;
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	padding: 3rem;
+	.container {
+		display: flex;
+		flex-direction: column;
+	}
+	.map {
+		height: 60vh;
+		background-color: brown;
+		width: 100vh;
+	}
 `;
