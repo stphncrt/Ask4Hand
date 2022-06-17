edc17e13967db56f1522b837b6ca63b7892d2c8a
ayktldg <aykutuludag26@gmail.com>
2022-06-17 15:18:48 +0200

    add no result message for search worker

--- client/src/pages/SearchPage/SearchPage.jsx
+++ client/src/pages/SearchPage/SearchPage.jsx
@@ -8,7 +8,7 @@ import WorkerInfoCard from "../../components/WorkerInfoCard";
 
 const libraries = ["places"];
 function SearchPage() {
-	const { isLoaded, loadError } = useLoadScript({
+  const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
     libraries,
   });
@@ -19,53 +19,63 @@ function SearchPage() {
     getWorkerByOccupation("/search", occupationIds);
   }, []);
 
-	return (
-		<StyledWrapper>
-			<div className="flex-row">
-				<TitleCheckBox />
-				<div className="map">
-					{loadError ? "error" : !isLoaded ? "loading..." : <Map workerList={workerList} />}
-				</div>
-			</div>
+  return (
+    <StyledWrapper>
+      <div className="flex-row">
+        <TitleCheckBox />
+        <div className="map">
+          {loadError ? (
+            "error"
+          ) : !isLoaded ? (
+            "loading..."
+          ) : (
+            <Map workerList={workerList} />
+          )}
+        </div>
+      </div>
 
-			<div className="flex-row">
-				{isLoading ? (
-					<h3>Loading...</h3>
-				) : (
-					workerList?.map((worker) => <WorkerInfoCard key = {worker._id} worker={worker} />)
-				)}
-			</div>
-		</StyledWrapper>
-	);
+      <div className="flex-row">
+        {isLoading ? (
+          <h3>Loading...</h3>
+        ) : workerList?.length === 0 ? (
+          <h3>There is no worker that fits your criteria.</h3>
+        ) : (
+          workerList?.map((worker) => (
+            <WorkerInfoCard key={worker._id} worker={worker} />
+          ))
+        )}
+      </div>
+    </StyledWrapper>
+  );
 }
 
 export default SearchPage;
 export const StyledWrapper = styled.div`
-	display: flex;
-	flex-wrap: wrap;
-	padding: 3rem;
-	align-items: flex-start;
-	gap: 1rem;
-	.container {
-		display: flex;
-		flex-direction: column;
-		background-color: red;
-		gap: 1rem;
-	}
-	.flex-row {
-		display: flex;
-		flex-direction: row;
-		gap: 1rem;
-		flex-wrap: wrap;
-	}
-	.map {
-		height: 480px;
-		background-color: #1976d2;
-		width: 60vw;
-	}
-	@media (max-width: 600px) {
-		.map {
-			width: 70vw;
-		}
-	}
+  display: flex;
+  flex-wrap: wrap;
+  padding: 3rem;
+  align-items: flex-start;
+  gap: 1rem;
+  .container {
+    display: flex;
+    flex-direction: column;
+    background-color: red;
+    gap: 1rem;
+  }
+  .flex-row {
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+    flex-wrap: wrap;
+  }
+  .map {
+    height: 480px;
+    background-color: #1976d2;
+    width: 60vw;
+  }
+  @media (max-width: 600px) {
+    .map {
+      width: 70vw;
+    }
+  }
 `;
