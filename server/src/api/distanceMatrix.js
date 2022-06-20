6dfd5d9e985d554c706a70e94412495f63524e93
ensar dogan <doganermansur@icloud.com>
2022-06-20 15:45:54 +0200

    search by workrange

--- server/src/api/distanceMatrix.js
+++ server/src/api/distanceMatrix.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
-export const distanceMatrix = async () => {
-  const URL =
-    "https://maps.googleapis.com/maps/api/distancematrix/json?destinations=Amsterdam&origins=Istanbul&units=imperial&key=AIzaSyA32MZKuao0P3ehpscNxbc7TZvMDSXJSFA";
-  const response = await axios.get(URL);
-  return response.data.rows[0].elements[0].distance.value;
+export const distanceMatrix = async (origin, destination) => {
+  try {
+    const URL = `https://maps.googleapis.com/maps/api/distancematrix/json?destinations=${origin}&origins=${destination}&units=imperial&key=${process.env.GOOGLE_MAP_API_KEY}`;
+    const response = await axios.get(URL);
+    const distance = Number.parseInt(
+      response.data.rows[0].elements[0].distance?.value / 1000
+    );
+    return distance;
+  } catch (err) {
+    console.log(err);
+  }
 };
--- server/src/controllers/workersController.js
+++ server/src/controllers/workersController.js
@@ -25,14 +25,30 @@ export const getWorkersByFilter = async (req, res) => {
   try {
     const workers = await Worker.find({
       occupationId: { $in: req.body.occupationIds },
-      city: { $regex: regex },
-    }).sort({ hourlyRate: 1 });
+    });
+    const workersWithDistance = await Promise.all(
+      workers.map(async (worker) => {
+        const distance = await distanceMatrix(regex, worker.city);
+        const newWorker = await { ...worker, distance };
+
+        return await newWorker;
+      })
+    );
+
+    const filteredWorkers = workersWithDistance
+      .filter((worker) => {
+        if (worker._doc.workRange >= worker.distance) {
+          return worker;
+        }
+      })
+      .map((worker) => {
+        const workerWithDistance = worker._doc;
+        return { ...workerWithDistance, distance: worker.distance };
+      });
     res.status(200).json({
       success: true,
-      result: workers,
+      result: filteredWorkers,
     });
-    const distance = await distanceMatrix();
-    console.log(distance);
   } catch (error) {
     logError(error);
     res.status(500).json({
