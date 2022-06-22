71a1dfea328bc56e3314b18acdc521d8905847aa
stphn <cretestephen@gmail.com>
2022-06-22 20:13:54 +0200

    conflicts fixed

--- client/src/components/DescriptionSection.jsx
+++ client/src/components/DescriptionSection.jsx
@@ -5,35 +5,32 @@ import flooring from "../../assets/flooring.jpeg";
 import cleanerLady from "../../assets/cleaner2.jpeg";
 
 function DescriptionSection() {
-  return (
-    <StyledContainer>
-      <div className="descriptionSection">
-        <div className="imageContainer">
-          <div className="columnImages">
-            <img src={flooring} alt="" />
-            <img src={cleanerLady} alt="" />
-          </div>
-          <img id="cleanWindow" src={cleaner} alt="" />
-        </div>
-        <div className="description">
-          <h2>About Our Company</h2>
-          <p>
-            We ensure the best quality clean at the best price based on your
-            budget. We do not stop until you are 100% satisfied with the results
-            of our services. Worrying about cleaning is at the bottom of your
-            priority list, but the top of ours. Our cleaners are trained to help
-            provide the best version of your space by focusing on the little
-            details..
-          </p>
-        </div>
-      </div>
-    </StyledContainer>
-  );
+	return (
+		<StyledContainer>
+			<div className="descriptionSection">
+				<div className="imageContainer">
+					<div className="columnImages">
+						<img src={flooring} alt="" />
+						<img src={cleanerLady} alt="" />
+					</div>
+					<img id="cleanWindow" src={cleaner} alt="" />
+				</div>
+				<div className="description">
+					<h2>About Our Company</h2>
+					<p>
+						We ensure the best quality clean at the best price based on your budget. We do not stop
+						until you are 100% satisfied with the results of our services. Worrying about cleaning
+						is at the bottom of your priority list, but the top of ours. Our cleaners are trained to
+						help provide the best version of your space by focusing on the little details..
+					</p>
+				</div>
+			</div>
+		</StyledContainer>
+	);
 }
 
 export default DescriptionSection;
 export const StyledContainer = styled.div`
-
 	background-color: #ebf1e9;
 	padding: 1rem;
 	display: flex;
@@ -53,11 +50,11 @@ export const StyledContainer = styled.div`
 		height: auto;
 		margin: 0 0.5rem;
 
-    .imageContainer {
-      display: flex;
-      flex-direction: row;
-      gap: 0.5rem;
-      
+		.imageContainer {
+			display: flex;
+			flex-direction: row;
+			gap: 0.5rem;
+
 			.columnImages {
 				display: flex;
 				flex-direction: column;
@@ -95,4 +92,4 @@ export const StyledContainer = styled.div`
 			}
 		}
 	}
-
+`;
