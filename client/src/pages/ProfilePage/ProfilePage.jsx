ace29c8322d8c7c027b2cf18383c5ad49e6bb1c4
ensar dogan <doganermansur@icloud.com>
2022-06-26 01:03:46 +0200

    modified profile page

--- client/src/pages/ProfilePage/ProfilePage.jsx
+++ client/src/pages/ProfilePage/ProfilePage.jsx
@@ -6,7 +6,7 @@ import WorkerForm from "../../components/WorkerForm";
 import Modal from "@mui/material/Modal";
 import { Box, Button } from "@material-ui/core";
 import styled from "styled-components";
-import { Grid } from "@mui/material";
+import { Grid, Card, CardActionArea, CardContent } from "@mui/material";
 import Carousel from "../../components/Carousel";
 
 const ProfilePage = () => {
@@ -15,8 +15,8 @@ const ProfilePage = () => {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const workerTitle = titles?.find(
-    (title) => title._id === worker.occupationId
-  ).name;
+    (title) => title._id === worker?.occupationId
+  )?.name;
 
   const style = {
     position: "absolute",
@@ -31,101 +31,118 @@ const ProfilePage = () => {
   };
 
   return (
-    // <StyledWrapper>
-    //   <Box class="Box-Container">
-    //     <Avatar src={worker?.profileImage} class="avatar"></Avatar>
-    //     <Container class="typography">
-    //       <Typography variant="h5">
-    //         Name: {worker?.firstName} {worker?.lastName}
-    //       </Typography>
-    //       <Typography variant="h5">Title: {workerTitle}</Typography>
-    //       <Typography variant="h5">City: {worker?.city}</Typography>
-    //       <Typography variant="h5">E-mail: {worker?.email}</Typography>
-    //       <Typography variant="h5">
-    //         Phone Number: {worker?.phoneNumber}
-    //       </Typography>
-    //     </Container>
-    //   </Box>
-    //   <Typography variant="h6" p={2}>
-    //     Description: {worker?.description}
-    //   </Typography>
-    //   <Button
-    //     style={{ maxWidth: "100px", textAlign: "center" }}
-    //     type="submit"
-    //     variant="contained"
-    //     color="primary"
-    //     onClick={handleOpen}
-    //   >
-    //     Edit
-    //   </Button>
-    //   <Modal
-    //     open={open}
-    //     onClose={handleClose}
-    //     aria-labelledby="modal-modal-title"
-    //     aria-describedby="modal-modal-description"
-    //   >
-    //     <Box sx={style}>
-    //       <WorkerForm />
-    //     </Box>
-    //   </Modal>
-    // </StyledWrapper>
-    <Box sx={{ flexGrow: 1, padding: "5rem 3rem" }}>
-      <Grid container spacing={5} gap={1}>
-        <Grid
-          item
-          xs={12}
-          md={4}
-          sx={{ boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px" }}
+    <Box sx={{ flexGrow: 1, padding: "2rem" }}>
+      <Grid container>
+        <Button
+          size="large"
+          style={{
+            margin: "2rem auto",
+          }}
+          type="submit"
+          variant="contained"
+          color="primary"
+          onClick={handleOpen}
         >
-          <Avatar src={worker?.profileImage} class="avatar"></Avatar>
+          Edit Profile
+        </Button>
+      </Grid>
+      <Avatar
+        src={worker?.profileImage}
+        style={{
+          width: 200,
+          height: 200,
+          margin: "2rem auto",
+          marginTop: 0,
+        }}
+      ></Avatar>
+      <Typography gutterBottom variant="h4" align="center" color="primary">
+        {worker?.firstName} {worker?.lastName}
+      </Typography>
+      <Grid container spacing={10} style={{ padding: "0rem 3rem" }}>
+        <Grid item xs={12} md={6}>
+          <Card sx={{ height: "33rem" }}>
+            <CardActionArea>
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                  color="primary"
+                  align="center"
+                >
+                  Previous Work Photos
+                </Typography>
+                <div
+                  style={{
+                    margin: "2rem",
+                  }}
+                >
+                  <Carousel images={worker?.images} />
+                </div>
+              </CardContent>
+            </CardActionArea>
+          </Card>
         </Grid>
-        <Grid
-          item
-          xs={12}
-          md={4}
-          justifyContent="center"
-          sx={{ boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px" }}
-        >
-          <Typography variant="h5">
-            Name: {worker?.firstName} {worker?.lastName}{" "}
-          </Typography>
-          <Typography variant="h6">Title: {workerTitle}</Typography>
-          <Typography variant="h6">City: {worker?.city}</Typography>
-          <Typography variant="h6">E-mail: {worker?.email}</Typography>
-          <Typography variant="h6">
-            Phone Number: {worker?.phoneNumber}
-          </Typography>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          md={4}
-          sx={{ boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px" }}
-        >
-          <Typography variant="h6" p={2}>
-            About me
-          </Typography>
-          <Typography>{worker?.description}</Typography>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          md={6}
-          justifyContent="center"
-          sx={{ height: "400px" }}
-        >
-          <Carousel images={worker?.images} />
-        </Grid>
-
-        <Grid item xs={6} md={3} lg={1} justifyContent="center">
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            onClick={handleOpen}
-          >
-            Edit
-          </Button>
+        <Grid item xs={12} md={6}>
+          <Card sx={{ height: "33rem" }}>
+            <CardActionArea>
+              <CardContent>
+                <Typography
+                  style={{ marginBottom: "2rem" }}
+                  variant="h5"
+                  component="div"
+                  color="primary"
+                  align="center"
+                >
+                  Credentials
+                </Typography>
+                <Typography gutterBottom variant="h5">
+                  Title: {workerTitle}
+                </Typography>
+                <Typography gutterBottom variant="h5">
+                  City: {worker?.city}
+                </Typography>
+                <Typography gutterBottom variant="h5">
+                  E-mail: {worker?.email}
+                </Typography>
+                <Typography gutterBottom variant="h5">
+                  Phone Number: {worker?.phoneNumber}
+                </Typography>
+                <Typography gutterBottom variant="h5">
+                  Address : {worker?.postalCode} {worker?.city}
+                </Typography>
+                <Typography gutterBottom variant="h5">
+                  Hourly Rate : {worker?.hourlyRate}
+                </Typography>
+                <Typography gutterBottom variant="h5">
+                  Work Range : {worker?.workRange}
+                </Typography>
+                <Typography
+                  variant="h6"
+                  color="text.secondary"
+                  style={{ paddingBottom: "4rem" }}
+                >
+                  {worker?.description}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+          {/* <Grid container>
+            <Grid item xs={12} md={6} justifyContent="center">
+              <Typography variant="h6">Title: {workerTitle}</Typography>
+              <Typography variant="h6">City: {worker?.city}</Typography>
+              <Typography variant="h6">E-mail: {worker?.email}</Typography>
+              <Typography variant="h6">
+                Phone Number: {worker?.phoneNumber}
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Typography variant="h6" p={2}>
+                About me
+              </Typography>
+              <Typography>{worker?.description}</Typography>
+            </Grid>
+          </Grid> */}
         </Grid>
       </Grid>
       <Modal
@@ -157,9 +174,9 @@ export const StyledWrapper = styled(Box)`
     align-items: flex-start;
     padding: 1rem;
   }
-  .avatar {
-    width: 20%;
-  }
+  /* .avatar {
+    width: %20;
+  } */
   .typography {
     width: 80%;
     display: flex;
