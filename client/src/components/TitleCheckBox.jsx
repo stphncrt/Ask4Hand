08a4f521bcdff6feb852ae7719628916d61ad48a
ShamaniCoder <ertugrulaktn@gmail.com>
2022-06-26 00:22:34 +0200

    add titlecheckbx styling

--- client/src/components/TitleCheckBox.jsx
+++ client/src/components/TitleCheckBox.jsx
@@ -8,74 +8,83 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 
 export default function CheckboxLabels() {
-	const {
-		titles,
-		getTitles,
-		getWorkers,
-		isLoading,
-		occupationIds,
-		setOccupationIds,
-		city,
-		setCity,
-	} = useContext(AppContext);
+  const {
+    titles,
+    getTitles,
+    getWorkers,
+    isLoading,
+    occupationIds,
+    setOccupationIds,
+    city,
+    setCity,
+  } = useContext(AppContext);
 
-	const handleChange = (e) => {
-		const { value, checked } = e.target;
-		// Case 1 : The user checks the box
-		if (checked) {
-			setOccupationIds([...occupationIds, value]);
-		}
-		// Case 2  : The user unchecks the box
-		else {
-			setOccupationIds(occupationIds.filter((id) => id !== value));
-		}
-	};
+  const handleChange = (e) => {
+    const { value, checked } = e.target;
+    // Case 1 : The user checks the box
+    if (checked) {
+      setOccupationIds([...occupationIds, value]);
+    }
+    // Case 2  : The user unchecks the box
+    else {
+      setOccupationIds(occupationIds.filter((id) => id !== value));
+    }
+  };
 
-	const handleSearch = () => {
-		getWorkers("/filter");
-	};
+  const handleSearch = () => {
+    getWorkers("/filter");
+  };
 
-	useEffect(() => {
-		getTitles("/occupations");
-		return () => {
-			setOccupationIds([]);
-			setCity("");
-		};
-	}, []);
+  useEffect(() => {
+    getTitles("/occupations");
+    return () => {
+      setOccupationIds([]);
+      setCity("");
+    };
+  }, []);
 
-	return (
-		<StyledFormGroup>
-			<div className="form-group">
-				{isLoading ? (
-					<h3>Loading..</h3>
-				) : (
-					titles?.map((title) => (
-						<FormControlLabel
-							key={title._id}
-							control={
-								occupationIds.includes(title._id) ? (
-									<Checkbox checked value={title._id} name="title" onChange={handleChange} />
-								) : (
-									<Checkbox value={title._id} name="title" onChange={handleChange} />
-								)
-							}
-							label={title.name}
-						/>
-					))
-				)}
-			</div>
-			<TextField
-				id="outlined-basic"
-				label="City"
-				variant="outlined"
-				value={city}
-				onChange={(e) => setCity(e.target.value)}
-			/>
-			<Button variant="contained" onClick={handleSearch}>
-				Search
-			</Button>
-		</StyledFormGroup>
-	);
+  return (
+    <StyledFormGroup>
+      <div className="form-group">
+        {isLoading ? (
+          <h3>Loading..</h3>
+        ) : (
+          titles?.map((title) => (
+            <FormControlLabel
+              key={title._id}
+              control={
+                occupationIds.includes(title._id) ? (
+                  <Checkbox
+                    checked
+                    value={title._id}
+                    name="title"
+                    onChange={handleChange}
+                  />
+                ) : (
+                  <Checkbox
+                    value={title._id}
+                    name="title"
+                    onChange={handleChange}
+                  />
+                )
+              }
+              label={title.name}
+            />
+          ))
+        )}
+      </div>
+      <TextField
+        id="outlined-basic"
+        label="City"
+        variant="outlined"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <Button variant="contained" onClick={handleSearch}>
+        Search
+      </Button>
+    </StyledFormGroup>
+  );
 }
 
 export const StyledFormGroup = styled.div`
