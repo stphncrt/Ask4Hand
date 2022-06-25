90c996d33678610f534217dc964831d4b10db417
stphn <cretestephen@gmail.com>
2022-06-25 14:04:33 +0200

    searchPage media query added

--- client/src/components/WorkerInfoCard.jsx
+++ client/src/components/WorkerInfoCard.jsx
@@ -11,69 +11,62 @@ import ModalCard from "./ModalCard";
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
-  container: {
-    position: "relative",
-    boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-    height: "100%",
-  },
-  desc: {
-    marginTop: "0",
-    marginBottom: "10px",
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-    display: "-webkit-box",
-    "	-webkit-line-clamp": "5" /* number of lines to show */,
-    "	-webkit-box-orient": "vertical",
-  },
-  modal: {
-    position: "absolute",
-    bottom: "1px",
-    paddingBottom: "0px",
-  },
+	container: {
+		position: "relative",
+		boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+		height: "100%",
+	},
+	desc: {
+		marginTop: "0",
+		marginBottom: "10px",
+		overflow: "hidden",
+		textOverflow: "ellipsis",
+		display: "-webkit-box",
+		"	-webkit-line-clamp": "5" /* number of lines to show */,
+		"	-webkit-box-orient": "vertical",
+	},
+	modal: {
+		position: "absolute",
+		bottom: "1px",
+		paddingBottom: "0px",
+	},
 }));
 const Root = styled("div")(({ theme }) => ({
-  padding: theme.spacing(1),
-  [theme.breakpoints.down("sm")]: {
-    width: "32%",
-  },
-  [theme.breakpoints.up("sm")]: {
-    width: "32%",
-  },
+	padding: theme.spacing(1),
+	[theme.breakpoints.down("sm")]: {
+		width: "100%",
+	},
+	[theme.breakpoints.up("sm")]: {
+		width: "32%",
+	},
 }));
 
 export default function WorkerInfoCard({ worker, titles }) {
-  const classes = useStyles();
-  const selectedTitle = titles?.find(
-    (title) => title._id === worker.occupationId
-  );
+	const classes = useStyles();
+	const selectedTitle = titles?.find((title) => title._id === worker.occupationId);
 
-  return (
-    <Root>
-      <Card className={classes.container}>
-        <CardHeader
-          avatar={
-            <Avatar
-              src={worker.profileImage}
-              sx={{ bgcolor: red[500], width: 92, height: 92 }}
-              aria-label="recipe"
-            ></Avatar>
-          }
-          title={`${worker?.firstName} ${worker?.lastName}`}
-          subheader={selectedTitle.name}
-        />
-        <CardContent>
-          <Typography
-            className={classes.desc}
-            variant="body2"
-            color="text.secondary"
-          >
-            {worker?.description}
-          </Typography>
-        </CardContent>
-        <CardActions className={classes.modal}>
-          <ModalCard images={worker.images} />
-        </CardActions>
-      </Card>
-    </Root>
-  );
+	return (
+		<Root>
+			<Card className={classes.container}>
+				<CardHeader
+					avatar={
+						<Avatar
+							src={worker.profileImage}
+							sx={{ bgcolor: red[500], width: 92, height: 92 }}
+							aria-label="recipe"></Avatar>
+					}
+					title={`${worker?.firstName} ${worker?.lastName}`}
+					subheader={selectedTitle.name}
+				/>
+				<CardContent>
+					<Typography className={classes.desc} variant="body2" color="text.secondary">
+						{worker?.description}
+					</Typography>
+				</CardContent>
+				<CardActions className={classes.modal}>
+					<ModalCard images={worker.images} />
+				</CardActions>
+			</Card>
+		</Root>
+	);
 }
--- client/src/pages/SearchPage/SearchPage.jsx
+++ client/src/pages/SearchPage/SearchPage.jsx
@@ -10,109 +10,113 @@ import Stack from "@mui/material/Stack";
 
 const libraries = ["places"];
 function SearchPage() {
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
-    libraries,
-  });
-  const { workerList, isLoading, getTitles, getWorkers, titles } =
-    useContext(AppContext);
+	const { isLoaded, loadError } = useLoadScript({
+		googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
+		libraries,
+	});
+	const { workerList, isLoading, getTitles, getWorkers, titles } = useContext(AppContext);
 
-  const [currentPage, setCurrentPage] = React.useState(1);
+	const [currentPage, setCurrentPage] = React.useState(1);
 
-  const handleChange = (event, value) => {
-    setCurrentPage(value);
-  };
+	const handleChange = (event, value) => {
+		setCurrentPage(value);
+	};
 
-  useEffect(() => {
-    getTitles("/occupations");
-    getWorkers("/filter");
-  }, []);
+	useEffect(() => {
+		getTitles("/occupations");
+		getWorkers("/filter");
+	}, []);
 
-  const workersPerPage = 3;
-  const indexOfLastWorker = currentPage * workersPerPage;
-  const indexOfFirstWorker = indexOfLastWorker - workersPerPage;
-  const currentWorkers = workerList?.slice(
-    indexOfFirstWorker,
-    indexOfLastWorker
-  );
+	const workersPerPage = 3;
+	const indexOfLastWorker = currentPage * workersPerPage;
+	const indexOfFirstWorker = indexOfLastWorker - workersPerPage;
+	const currentWorkers = workerList?.slice(indexOfFirstWorker, indexOfLastWorker);
 
-  return (
-    <StyledWrapper>
-      <div className="flex-row first-section">
-        <TitleCheckBox />
-        <div className="map">
-          {loadError ? (
-            "error"
-          ) : !isLoaded ? (
-            "loading..."
-          ) : (
-            <Map workerList={workerList} />
-          )}
-        </div>
-      </div>
+	return (
+		<StyledWrapper>
+			<div className="flex-row first-section">
+				<TitleCheckBox />
+				<div className="map">
+					{loadError ? "error" : !isLoaded ? "loading..." : <Map workerList={workerList} />}
+				</div>
+			</div>
 
-      <div className="flex-row cards">
-        {isLoading ? (
-          <h3>Loading...</h3>
-        ) : currentWorkers?.length === 0 ? (
-          <h3>There is no worker that fits your criteria.</h3>
-        ) : (
-          currentWorkers?.map((worker) => (
-            <WorkerInfoCard key={worker._id} worker={worker} titles={titles} />
-          ))
-        )}
-      </div>
-      {workerList?.length > 0 && (
-        <Stack spacing={2}>
-          <Pagination
-            count={Math.ceil(workerList?.length / workersPerPage)}
-            currentPage={currentPage}
-            onChange={handleChange}
-          />
-        </Stack>
-      )}
-    </StyledWrapper>
-  );
+			<div className="flex-row cards">
+				{isLoading ? (
+					<h3>Loading...</h3>
+				) : currentWorkers?.length === 0 ? (
+					<h3>There is no worker that fits your criteria.</h3>
+				) : (
+					currentWorkers?.map((worker) => (
+						<WorkerInfoCard key={worker._id} worker={worker} titles={titles} />
+					))
+				)}
+			</div>
+			{workerList?.length > 0 && (
+				<Stack spacing={2}>
+					<Pagination
+						count={Math.ceil(workerList?.length / workersPerPage)}
+						currentPage={currentPage}
+						onChange={handleChange}
+					/>
+				</Stack>
+			)}
+		</StyledWrapper>
+	);
 }
 
 export default SearchPage;
 export const StyledWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  padding: 3rem;
-  align-items: flex-start;
-  gap: 1rem;
+	display: flex;
 
-  .container {
-    display: flex;
-    flex-direction: column;
-    background-color: red;
-    gap: 1rem;
-  }
-  .first-section {
-    height: 70vh;
-  }
-  .flex-row {
-    display: flex;
-    flex-direction: row;
-    gap: 1rem;
-    flex-wrap: wrap;
-    min-width: 95vw;
-  }
-  .cards {
-    justify-content: space-around;
-  }
-  .map {
-    height: 70vh;
-    background-color: #1976d2;
-    width: 68%;
-  }
-  .pagination {
-    justify-content: center;
-  }
-  @media (max-width: 600px) {
-    .map {
-      width: 70vw;
-    }
-  }
+	flex-wrap: wrap;
+	padding: 3rem;
+	align-items: flex-start;
+	gap: 1rem;
+
+	.container {
+		display: flex;
+		flex-direction: column;
+		background-color: red;
+		gap: 1rem;
+	}
+	.first-section {
+		height: 70vh;
+	}
+	.flex-row {
+		display: flex;
+		flex-direction: row;
+		gap: 1rem;
+		flex-wrap: wrap;
+		min-width: 95vw;
+	}
+	.cards {
+		justify-content: space-around;
+	}
+	.map {
+		height: 70vh;
+		background-color: #1976d2;
+		width: 68%;
+	}
+	.pagination {
+		justify-content: center;
+	}
+	@media (max-width: 600px) {
+		flex-direction: column;
+		margin: 0;
+		padding: 0;
+		gap: 9rem;
+		.map {
+			width: 100%;
+			height: 40vh;
+		}
+		.first-section {
+			width: 80%;
+			margin: 0;
+		}
+		.cards {
+			display: flex;
+			flex-direction: column;
+		}
+	}
 `;
