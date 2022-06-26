4d0d8e57fd6891993dbb0062d95138dbde2ef20d
ensar dogan <doganermansur@icloud.com>
2022-06-26 10:32:34 +0200

    modified login page

--- client/src/pages/Login/LoginPage.jsx
+++ client/src/pages/Login/LoginPage.jsx
@@ -4,12 +4,15 @@ import { useFormik } from "formik";
 import AppContext from "../../context/AppContext";
 import { Button, TextField, Grid, Container, Typography } from "@mui/material";
 import * as Yup from "yup";
+import styled from "styled-components";
+import searchBackgroundImage from "../../../assets/search-bar-background.jpeg";
+import { StyledWrapper } from "../RegisterPage/RegisterPage";
 
 const styleFunc = makeStyles({
   wrapper: {
     margin: "3rem auto",
     boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
-    padding: "1rem",
+    padding: "5rem",
     backgroundColor: "rgb(241,250,238)",
   },
   text: {
@@ -47,58 +50,60 @@ const LoginPage = () => {
   });
 
   return (
-    <Container className={signUpStyles.wrapper} maxWidth="sm">
-      <Typography className={signUpStyles.text}>
-        Pleaser enter your email and password to login..
-      </Typography>
-      <form onSubmit={formik.handleSubmit}>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <TextField
-              label="Email"
-              variant="outlined"
-              fullWidth
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              name="email"
-            />
-          </Grid>
-          {formik.touched.email && formik.errors.email ? (
-            <Grid item xs={12} style={{ color: "red" }}>
-              {formik.errors.email}
+    <StyledWrapper>
+      <Container className={signUpStyles.wrapper} maxWidth="sm">
+        <Typography className={signUpStyles.text}>
+          Pleaser enter your email and password to login..
+        </Typography>
+        <form onSubmit={formik.handleSubmit}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <TextField
+                label="Enter Your Email"
+                variant="outlined"
+                fullWidth
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                name="email"
+              />
             </Grid>
-          ) : null}
+            {formik.touched.email && formik.errors.email ? (
+              <Grid item xs={12} style={{ color: "red" }}>
+                {formik.errors.email}
+              </Grid>
+            ) : null}
 
-          <Grid item xs={12}>
-            <TextField
-              label="Create Password"
-              variant="outlined"
-              fullWidth
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              name="password"
-              type="password"
-            />
-          </Grid>
-          {formik.touched.password && formik.errors.password ? (
-            <Grid item xs={12} style={{ color: "red" }}>
-              {formik.errors.password}
+            <Grid item xs={12}>
+              <TextField
+                label="Enter Your Password"
+                variant="outlined"
+                fullWidth
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                name="password"
+                type="password"
+              />
             </Grid>
-          ) : null}
-          <Grid item xs={12}>
-            <Button
-              type="submit"
-              disabled={formik.isSubmitting}
-              variant="contained"
-              color="primary"
-              fullWidth
-            >
-              {formik.isSubmitting ? "Checking" : "Login"}
-            </Button>
+            {formik.touched.password && formik.errors.password ? (
+              <Grid item xs={12} style={{ color: "red" }}>
+                {formik.errors.password}
+              </Grid>
+            ) : null}
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                disabled={formik.isSubmitting}
+                variant="contained"
+                color="primary"
+                fullWidth
+              >
+                {formik.isSubmitting ? "Checking" : "Login"}
+              </Button>
+            </Grid>
           </Grid>
-        </Grid>
-      </form>
-    </Container>
+        </form>
+      </Container>
+    </StyledWrapper>
   );
 };
 
