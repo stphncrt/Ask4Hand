3a0446a7e91279cb9e962721360490af94ec2729
ayktldg <aykutuludag26@gmail.com>
2022-06-29 16:12:14 +0200

    fix linting

--- client/src/pages/Login/LoginPage.jsx
+++ client/src/pages/Login/LoginPage.jsx
@@ -1,83 +1,84 @@
 import React, { useContext } from "react";
 import { useFormik } from "formik";
 import AppContext from "../../context/AppContext";
-import { Button, TextField, Grid, Container, Typography } from "@mui/material";
+import { Button, TextField, Grid, Typography } from "@mui/material";
 import { LoginValidationSchema } from "../../util/formValidation";
 import { StyledWrapper } from "../RegisterPage/RegisterPage";
 import { StyledContainer } from "../../components/WorkerForm";
 
 const LoginPage = () => {
-	const { postWorker } = useContext(AppContext);
-	const formik = useFormik({
-		initialValues: {
-			email: "",
-			password: "",
-		},
-		validationSchema: LoginValidationSchema,
-		onSubmit: (values, props) => {
-			postWorker("/auth/login", values);
-			setTimeout(() => {
-				props.resetForm();
-				props.setSubmitting(false);
-			}, 500);
-		},
-	});
+  const { postWorker } = useContext(AppContext);
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: LoginValidationSchema,
+    onSubmit: (values, props) => {
+      postWorker("/auth/login", values);
+      setTimeout(() => {
+        props.resetForm();
+        props.setSubmitting(false);
+      }, 500);
+    },
+  });
 
-	return (
-		<StyledWrapper>
-			<StyledContainer>
-				<Typography align="center" m={2} variant="h6">
-					Pleaser enter your email and password to login..
-				</Typography>
-				<form onSubmit={formik.handleSubmit}>
-					<Grid container spacing={3}>
-						<Grid item xs={12}>
-							<TextField
-								label="Enter Your Email"
-								variant="outlined"
-								fullWidth
-								value={formik.values.email}
-								onChange={formik.handleChange}
-								name="email"
-							/>
-						</Grid>
-						{formik.touched.email && formik.errors.email ? (
-							<Grid item xs={12} style={{ color: "red" }}>
-								{formik.errors.email}
-							</Grid>
-						) : null}
+  return (
+    <StyledWrapper>
+      <StyledContainer>
+        <Typography align="center" m={2} variant="h6">
+          Pleaser enter your email and password to login..
+        </Typography>
+        <form onSubmit={formik.handleSubmit}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <TextField
+                label="Enter Your Email"
+                variant="outlined"
+                fullWidth
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                name="email"
+              />
+            </Grid>
+            {formik.touched.email && formik.errors.email ? (
+              <Grid item xs={12} style={{ color: "red" }}>
+                {formik.errors.email}
+              </Grid>
+            ) : null}
 
-						<Grid item xs={12}>
-							<TextField
-								label="Enter Your Password"
-								variant="outlined"
-								fullWidth
-								value={formik.values.password}
-								onChange={formik.handleChange}
-								name="password"
-								type="password"
-							/>
-						</Grid>
-						{formik.touched.password && formik.errors.password ? (
-							<Grid item xs={12} style={{ color: "red" }}>
-								{formik.errors.password}
-							</Grid>
-						) : null}
-						<Grid item xs={12}>
-							<Button
-								type="submit"
-								disabled={formik.isSubmitting}
-								variant="contained"
-								color="primary"
-								fullWidth>
-								{formik.isSubmitting ? "Checking" : "Login"}
-							</Button>
-						</Grid>
-					</Grid>
-				</form>
-			</StyledContainer>
-		</StyledWrapper>
-	);
+            <Grid item xs={12}>
+              <TextField
+                label="Enter Your Password"
+                variant="outlined"
+                fullWidth
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                name="password"
+                type="password"
+              />
+            </Grid>
+            {formik.touched.password && formik.errors.password ? (
+              <Grid item xs={12} style={{ color: "red" }}>
+                {formik.errors.password}
+              </Grid>
+            ) : null}
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                disabled={formik.isSubmitting}
+                variant="contained"
+                color="primary"
+                fullWidth
+              >
+                {formik.isSubmitting ? "Checking" : "Login"}
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </StyledContainer>
+    </StyledWrapper>
+  );
 };
 
 export default LoginPage;
--- client/src/pages/RegisterPage/RegisterPage.jsx
+++ client/src/pages/RegisterPage/RegisterPage.jsx
@@ -4,24 +4,24 @@ import searchBackgroundImage from "../../../assets/search-bar-background.jpeg";
 import WorkerForm from "../../components/WorkerForm";
 
 const RegisterPage = () => {
-	return (
-		<>
-			<StyledWrapper>
-				<WorkerForm />
-			</StyledWrapper>
-		</>
-	);
+  return (
+    <>
+      <StyledWrapper>
+        <WorkerForm />
+      </StyledWrapper>
+    </>
+  );
 };
 
 export default RegisterPage;
 
 export const StyledWrapper = styled.div`
-	padding: 5rem;
-	background-image: linear-gradient(rgba(0, 0, 0, 0.527), rgba(0, 0, 0, 0.5)),
-		url(${searchBackgroundImage});
-	background-repeat: no-repeat;
-	background-size: cover;
-	@media screen and (max-width: 600px) {
-		padding: 3rem 1.5rem;
-	}
+  padding: 5rem;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.527), rgba(0, 0, 0, 0.5)),
+    url(${searchBackgroundImage});
+  background-repeat: no-repeat;
+  background-size: cover;
+  @media screen and (max-width: 600px) {
+    padding: 3rem 1.5rem;
+  }
 `;
