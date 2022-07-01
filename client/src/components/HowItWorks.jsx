3c39dab93cdc576931397b1ad81e2372ee515813
stphn <cretestephen@gmail.com>
2022-07-02 00:28:09 +0200

    howitworksModified

--- client/src/components/HowItWorks.jsx
+++ client/src/components/HowItWorks.jsx
@@ -1,104 +1,73 @@
+import { Typography, Grid } from "@mui/material";
 import React from "react";
-import styled from "styled-components";
 import logoTell from "../../assets/hiw1.jpeg";
 import logoReview from "../../assets/hiw2.jpeg";
 import logoHire from "../../assets/hiw3.jpeg";
+import { makeStyles } from "@mui/styles";
 
+const useStyles = makeStyles({
+	ImgContainer: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+		width: "110px",
+		height: "110px",
+		borderRadius: "50%",
+		backgroundColor: "#fff",
+		" -webkit-box-shadow": " 2px 3px 30px rgb(188 207 219 / 65%)",
+		"-moz-box-shadow": "2px 3px 30px rgba(188, 207, 219, 0.65)",
+		boxShadow: "2px 3px 30px rgb(188 207 219 / 65%)",
+		lineHeight: " 110px",
+		margin: "0 auto",
+	},
+});
 function HowItWorks() {
-  return (
-    <StyledContainer>
-      <h2 className="title">How It Works</h2>
-      <div className="stepperWrapper">
-        <div className="step">
-          <div className="imgContainer">
-            <img src={logoTell} />
-          </div>
-          <h3>Tell us what you need</h3>
-          <p>
-            Book & pay online. We’ll match you with a trusted, experienced house
-            cleaner
-          </p>
-        </div>
-        <div className="step">
-          <div className="imgContainer">
-            <img src={logoReview} />
-          </div>
-          <h3>Review service providers</h3>
-          <p>
-            Within seconds, you’ll receive expert service providers profile with
-            their ratings. choose one among them.
-          </p>
-        </div>
-        <div className="step">
-          <div className="imgContainer">
-            <img src={logoHire} />
-          </div>
-          <h3>Hire the right pro</h3>
-          <p>
-            Compare prices, profiles, and reviews, then hire the pro that’s
-            right for you.
-          </p>
-        </div>
-      </div>
-    </StyledContainer>
-  );
+	const classes = useStyles();
+	return (
+		<Grid container bgcolor={" #f1faee"} p={3}>
+			<Grid item xs={12} align="center">
+				<Typography variant="h5" color="primary" mb={2} sx={{ fontWeight: "600" }}>
+					How It Works
+				</Typography>
+			</Grid>
+			<Grid container spacing={2}>
+				<Grid item xs={12} md={4}>
+					<Grid className={classes.ImgContainer}>
+						<img src={logoTell} />
+					</Grid>
+					<Typography align="center" variant="h6" color="primary" m={2} sx={{ fontWeight: "600" }}>
+						Tell us what you need
+					</Typography>
+					<Typography align="center" variant="body1">
+						Book & pay online. We’ll match you with a trusted, experienced house cleaner{" "}
+					</Typography>
+				</Grid>
+				<Grid item xs={12} md={4}>
+					<Grid className={classes.ImgContainer}>
+						<img src={logoReview} />
+					</Grid>
+					<Typography align="center" variant="h6" color="primary" m={2} sx={{ fontWeight: "600" }}>
+						Review service providers
+					</Typography>
+					<Typography align="center" variant="body1">
+						Within seconds, you’ll receive expert service providers profile with their ratings.
+						choose one among them.
+					</Typography>
+				</Grid>
+				<Grid item xs={12} md={4}>
+					<Grid className={classes.ImgContainer}>
+						<img src={logoHire} />
+					</Grid>
+					<Typography align="center" variant="h6" color="primary" m={2} sx={{ fontWeight: "600" }}>
+						Hire the right pro
+					</Typography>
+					<Typography align="center" variant="body1">
+						Compare prices, profiles, and reviews, then hire the pro that’s right for you.
+					</Typography>
+				</Grid>
+			</Grid>
+		</Grid>
+	);
 }
 
 export default HowItWorks;
-export const StyledContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  text-align: center;
-  justify-content: center;
-  background-color: #f1faee;
-  padding: 3rem 0 5rem 0;
-
-  h2 {
-    text-align: center;
-    color: #1976d2;
-    /* margin-left: -4rem; */
-  }
-  h3 {
-    color: #1976d2;
-    font-weight: 600;
-  }
-  .stepperWrapper {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-  }
-  .step {
-    display: flex;
-    flex-direction: column;
-    width: 30%;
-    align-items: center;
-    justify-content: center;
-    p {
-      text-align: center;
-    }
-
-    .imgContainer {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      width: 110px;
-      height: 110px;
-      border-radius: 50%;
-      background-color: #fff;
-      -webkit-box-shadow: 2px 3px 30px rgb(188 207 219 / 65%);
-      -moz-box-shadow: 2px 3px 30px rgba(188, 207, 219, 0.65);
-      box-shadow: 2px 3px 30px rgb(188 207 219 / 65%);
-      line-height: 110px;
-    }
-  }
-
-  @media screen and (max-width: 600px) {
-    .step {
-      width: 100%;
-    }
-    h2 {
-      margin-left: 0;
-    }
-  }
-`;
